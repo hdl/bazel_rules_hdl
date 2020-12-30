c968c35220d9708d7500961856de5440ee73aa43
Per Grön <pgron@google.com>
2020-12-30 17:35:03 +0100

    Build and test all the things in CI!

---
+++ tools/test_everything.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import subprocess
+
+ALL_TARGETS = [
+  '//...',
+  '@at_clifford_icestorm//...',
+  '@at_clifford_yosys//...',
+  '@com_github_westes_flex//...',
+  '@com_github_yosyshq_nextpnr//...',
+  '@com_github_yosyshq_prjtrellis//...',
+  '@com_github_yosyshq_prjtrellis_db//...',
+  '@com_google_skywater_pdk//...',
+  '@com_icarus_iverilog//...',
+  '@com_opencircuitdesign_magic//...',
+  '@com_opencircuitdesign_netgen//...',
+  '@edu_berkeley_abc//...',
+  '@net_invisible_island_ncurses//...',
+  '@net_sourceforge_ngspice//...',
+  '@net_zlib//...',
+  '@org_fftw//...',
+  '@org_gnu_bison//...',
+  '@org_gnu_gperf//...',
+  '@org_gnu_m4//...',
+  '@org_gnu_readline//...',
+  '@org_sourceware_bzip2//...',
+  '@org_sourceware_libffi//...',
+  '@org_tuxfamily_eigen//...',
+  '@pybind11//...',
+  '@tk_tcl//...',
+]
+
+for action in ['build', 'test']:
+  subprocess.call('bazel ' + action + ' ' + os.environ.get('EXTRA_BAZEL_ARGS', '') + ' ' + ' '.join(ALL_TARGETS), shell=True)
+
