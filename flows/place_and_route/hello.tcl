bf2a81a921e27740876b0aa9dd3577ae076803c1
Ravi Nanavati <amfv@google.com>
2021-12-16 19:44:06 -0800

    Add an example of a two-step flow (and the fixes required to get it to work).

--- flows/place_and_route/hello.tcl
+++ flows/place_and_route/hello.tcl
@@ -1,1 +1,5 @@
-puts "Hello, Bazel and OpenROAD!"
+puts "hello.tcl"
+puts [concat "output file name: " $::env(OUTPUT_HELLO)]
+set output_file [open $::env(OUTPUT_HELLO) "w"]
+puts $output_file "Hello, Bazel and OpenROAD!"
+close $output_file
