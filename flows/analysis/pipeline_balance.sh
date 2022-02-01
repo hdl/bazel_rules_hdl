53b3a09aefca637ecf93afd1534ae387bf4e1f1f
Ravi Nanavati <amfv@google.com>
2022-02-01 15:01:53 -0800

    The pipeline balance script shouldn't run with compilation mode debug.

--- flows/analysis/pipeline_balance.sh
+++ flows/analysis/pipeline_balance.sh
@@ -3,5 +3,5 @@ export CONSTANT_CLOCK_PORT=clk
 export CONSTANT_TOP=$3
 export OUTPUT_NETLIST=`mktemp -p /tmp XXXXXXXX.v`
 export INPUT_RTL=$2
-time bazel run --compilation_mode=dbg //flows/analysis:pipeline_balance_$1
+time bazel run //flows/analysis:pipeline_balance_$1
 
