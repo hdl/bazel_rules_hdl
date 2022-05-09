eaedaa0cc5bb988f476cdca2895820d376a7dee3
Ethan Mahintorabi <ethanmoon@google.com>
2022-05-09 17:26:15 +0000

    Fixes libbacktrace support in rules_boost

--- tools/test_everything.py
+++ tools/test_everything.py
@@ -29,6 +29,7 @@ ALL_TARGETS = [
   '@com_github_westes_flex//...',
   '@com_github_fmtlib_fmt//...',
   '@com_github_gabime_spdlog//...',
+  '@com_github_libbacktrace//...',
   '@com_github_quantamhd_lemon//...',
   '@com_github_yosyshq_nextpnr//...',
   '@com_github_yosyshq_prjtrellis//...',
