d2df317db4bf0ffb35aa9e048e0d516d45b6edb3
Stephen Tridgell <stephen.tridgell@cruxml.com>
2022-10-13 17:26:03 +1100

    Clang format the cc files.

--- verilator/tests/load_and_count_test.cc
+++ verilator/tests/load_and_count_test.cc
@@ -9,17 +9,18 @@
 namespace {
 
 class LoadAndCountTest : public testing::Test {
-    protected:
-    void Clock(Vload_and_count* v_load_and_count) {
-        v_load_and_count->clk = 0;
-        v_load_and_count->eval();
-        v_load_and_count->clk = 1;
-        v_load_and_count->eval();
-    }
+ protected:
+  void Clock(Vload_and_count* v_load_and_count) {
+    v_load_and_count->clk = 0;
+    v_load_and_count->eval();
+    v_load_and_count->clk = 1;
+    v_load_and_count->eval();
+  }
 };
 
 TEST_F(LoadAndCountTest, count_three_times) {
-  std::unique_ptr<Vload_and_count> v_load_and_count = std::make_unique<Vload_and_count>();
+  std::unique_ptr<Vload_and_count> v_load_and_count =
+      std::make_unique<Vload_and_count>();
   v_load_and_count->eval();
   EXPECT_EQ(v_load_and_count->counter_value, 0xA5B6);
   Clock(v_load_and_count.get());
--- verilator/tests/nested_module_test.cc
+++ verilator/tests/nested_module_test.cc
@@ -9,25 +9,24 @@
 namespace {
 
 class NestedModuleTest : public testing::Test {
-    protected:
-    void Clock(Vnested_module_2* v_nested_module_2) {
-        v_nested_module_2->clk = 0;
-        v_nested_module_2->eval();
-        v_nested_module_2->clk = 1;
-        v_nested_module_2->eval();
-    }
-
-
+ protected:
+  void Clock(Vnested_module_2* v_nested_module_2) {
+    v_nested_module_2->clk = 0;
+    v_nested_module_2->eval();
+    v_nested_module_2->clk = 1;
+    v_nested_module_2->eval();
+  }
 };
 
 TEST_F(NestedModuleTest, test_all) {
-  std::unique_ptr<Vnested_module_2> v_nested_module_2 = std::make_unique<Vnested_module_2>();
+  std::unique_ptr<Vnested_module_2> v_nested_module_2 =
+      std::make_unique<Vnested_module_2>();
   // Run for 11 clock cycles, 8 test values and 3 cycles to propogate.
   for (int i = 0; i < 11; i++) {
     v_nested_module_2->input_val = i % 8;
     Clock(v_nested_module_2.get());
     if (i > 2) {
-        EXPECT_EQ(v_nested_module_2->output_val, (~(i - 2)) & 0x7);
+      EXPECT_EQ(v_nested_module_2->output_val, (~(i - 2)) & 0x7);
     }
   }
 }
