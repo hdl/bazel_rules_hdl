b32679e05450c0f851846c34e87431eb9c2ca8ef
bazel_rules_hdl team <no-reply@google.com>
2023-02-03 12:29:03 -0800

    Rules to run opensta to get longest path.
    Uses a default "sta.tcl" script that can be overridden.
    
    PiperOrigin-RevId: 506967205

---
+++ static_timing/sta.tcl
@@ -0,0 +1,26 @@
+# Default openSTA timing tcl script for the run_opensta rule.
+# It can be replaced by a user-defined script by overriding the sta_tcl
+# argument of that rule.
+
+# User-defined timing scripts need to consult the following environment
+# variables for their parameters:
+# NETLIST = synthesized netlist (Verilog)
+# TOP = top module in NETLIST
+# LIBERTY = liberty file for the target technology library
+# LOGFILE = file for analysis output
+
+
+set sta_log $::env(LOGFILE)
+set netlist $::env(NETLIST)
+set liberty $::env(LIBERTY)
+set top $::env(TOP)
+
+redirect_file_begin $sta_log
+read_verilog $netlist
+read_liberty $liberty
+link_design  $top
+report_checks -unconstrained
+redirect_file_end
+
+# also output to stdout
+report_checks -unconstrained
