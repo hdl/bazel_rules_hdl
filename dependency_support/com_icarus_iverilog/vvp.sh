0bc711312196b27c184262ed61b4f72211adbb5a
Johan Euphrosine <proppy@google.com>
2023-10-31 14:42:02 +0900

    dependency_support/com_icarus_iverilog: update bazel build

--- dependency_support/com_icarus_iverilog/vvp.sh
+++ dependency_support/com_icarus_iverilog/vvp.sh
@@ -18,12 +18,7 @@
 
 set -eu
 
-vvp_path="$(find . -name vvp-bin | head -n 1)"
-if [ "$vvp_path" == "" ]; then
-  vvp_path="$(command -v vvp-bin)"
-fi
-
-dir=$(dirname "$vvp_path")
+dir="$0.runfiles/com_icarus_iverilog"
 
 if [[ ! -d "$dir" ]]; then
   echo "Unable to find dependencies (looking under $dir)." 1>&2
