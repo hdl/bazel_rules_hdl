2262fabc8fce166dd8d4aec633cdb8d8c439c210
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-11-23 23:20:06 +0100

    fix final report

--- megaboom/private.mk
+++ megaboom/private.mk
@@ -88,7 +88,9 @@ check-route:
 .PHONY: bazel-final
 bazel-final:
 	mkdir -p $(RESULTS_DIR) $(LOG_DIR) $(REPORTS_DIR)
-	$(UNSET_AND_MAKE) check-route do-6_1_fill do-6_1_fill.sdc do-6_final.sdc do-6_report do-gds
+	$(UNSET_AND_MAKE) check-route do-6_1_fill do-6_1_fill.sdc do-6_final.sdc do-6_report
+	$(UNSET_AND_MAKE) do-klayout_tech do-klayout do-klayout_wrap do-gds-merged
+	cp $(GDS_MERGED_FILE) $(GDS_FINAL_FILE)
 
 .PHONY: bazel-generate_abstract
 bazel-generate_abstract:
